034c25252e637c9bc9d6afcef7b555f73c3da479
Yohualkis <Yohualkis99@gmail.com>
2024-11-14 02:11:40 -0400

    Se creo el contexto

---
+++ DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YohualkisTejada_AP1_P2.Models;
+
+namespace YohualkisTejada_AP1_P2.DAL;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options) { }
+
+    public DbSet<Modelos> MyProperty { get; set; }
+}
