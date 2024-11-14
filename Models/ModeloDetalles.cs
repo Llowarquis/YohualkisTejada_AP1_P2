11c37380c1ec7b2e62263da4e90dbddb48b68e00
Yohualkis <Yohualkis99@gmail.com>
2024-11-14 02:12:00 -0400

    Se creo el modelo Detalles

---
+++ Models/ModeloDetalles.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YohualkisTejada_AP1_P2.Models;
+
+public class ModeloDetalles
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public int Cantidad { get; set; }
+}
