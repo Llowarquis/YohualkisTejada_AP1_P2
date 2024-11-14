9f290926758b45f20151d0bac0fe50178c081b7c
Yohualkis <Yohualkis99@gmail.com>
2024-11-14 02:23:35 -0400

    Se creo la plantilla de servicios

---
+++ Services/ModelosService.cs
@@ -0,0 +1,50 @@
+﻿namespace YohualkisTejada_AP1_P2.Services;
+
+public class ModelosService
+{
+    // Guardar
+    public async Task Guardar() 
+    { 
+        
+    }
+
+	// Insertar
+	public async Task Insertar()
+	{
+
+	}
+
+	// Modificar
+	public async Task Modificar()
+	{
+
+	}
+
+	// Existe
+	public async Task Existe()
+	{
+
+	}
+
+	// Eliminar
+	public async Task Eliminar()
+	{
+
+	}
+
+
+	// Buscar
+	public async Task Buscar()
+	{
+
+	}
+
+
+	// Listar
+	public async Task Listar()
+	{
+
+	}
+
+
+}
