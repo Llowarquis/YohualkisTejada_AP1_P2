b765ef1f72f8ac031ee44318f8c51ca9b96cbf92
Yohualkis <Yohualkis99@gmail.com>
2024-11-14 02:12:17 -0400

    Se creo el modelo principal

---
+++ Models/Modelos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YohualkisTejada_AP1_P2.Models;
+
+public class Modelos
+{
+    [Key]
+    public int ModeloId { get; set; }
+
+	public string Nombres { get; set; } = string.Empty;
+
+	public int Sueldo { get; set; }
+
+	public ICollection<ModeloDetalles> Detalles { get; set; } = new List<ModeloDetalles>();
+}
