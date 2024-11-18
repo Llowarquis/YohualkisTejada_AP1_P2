29e0094782d8ae2de0198096627b4b24e2d2764f
Yohualkis <Yohualkis99@gmail.com>
2024-11-18 16:10:21 -0400

    Se creo la clase modelos

--- Models/Modelos.cs
+++ Models/Modelos.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace YohualkisTejada_AP1_P2.Models;
 
@@ -7,9 +8,17 @@ public class Modelos
     [Key]
     public int ModeloId { get; set; }
 
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
 	public string Nombres { get; set; } = string.Empty;
 
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
 	public int Sueldo { get; set; }
 
-	public ICollection<ModeloDetalles> Detalles { get; set; } = new List<ModeloDetalles>();
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public DateTime Fecha { get; set; } = DateTime.Now;
+
+	[ForeignKey("ModeloId")]
+    public ICollection<ModelosDetalles> Detalles { get; set; } = new List<ModelosDetalles>();
 }
