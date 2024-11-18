b2b9485971f11b98ec339588bfae4c5db9a93b87
Yohualkis <Yohualkis99@gmail.com>
2024-11-18 16:10:27 -0400

    Se creo el detalle

---
+++ Models/ModelosDetalles.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YohualkisTejada_AP1_P2.Models;
+
+public class ModelosDetalles
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public int Cantidad { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio")]
+	public double Precio { get; set; }
+
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [ForeignKey("Articulos")]
+	public int ArticuloId { get; set; }
+	public ArticulosModelos? Articulos { get; set; }
+}
