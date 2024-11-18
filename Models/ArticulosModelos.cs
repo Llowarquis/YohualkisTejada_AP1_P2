ca8d01fec0a7ebf1f9d3a2e9c082d5e3ce5d83be
Yohualkis <Yohualkis99@gmail.com>
2024-11-18 16:10:15 -0400

    Se creo la clase Articulos

---
+++ Models/ArticulosModelos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YohualkisTejada_AP1_P2.Models;
+
+public class ArticulosModelos // El nombre de esta clase esta sujeta a cambios en el 2do parcial -> 18/11/2024
+{
+    [Key]
+    public int ArticuloId { get; set; }
+	public string? Descripcion { get; set; }
+
+	public double Costo { get; set; }
+
+	public double Precio { get; set; }
+
+	public int Existencia { get; set; }
+}
